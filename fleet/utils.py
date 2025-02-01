da5e254e16eefdf69f3325e9e9ce877a593c6d98
Rajo Miliceivc <rajomilicevic2011@gmail.com>
2025-02-01 13:15:59 +0100

    proba

--- fleet/utils.py
+++ fleet/utils.py
@@ -1861,10 +1861,10 @@ def kerio_login(*args, **kwargs):
 
     # Opcije za Chrome
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
 
     # Initialize WebDriver
     driver = webdriver.Chrome(options=chrome_options)
