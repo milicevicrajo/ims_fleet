64d185c9a1b1154ac6e66b640933437fb8594ee9
milicevicrajo <rajomilicevic2011@gmail.com>
2025-10-01 13:16:58 +0200

    server db

--- ims_fleet/settings/production.py
+++ ims_fleet/settings/production.py
@@ -22,7 +22,7 @@ DATABASES = {
 
     'server_db': {
         'ENGINE': 'mssql',
-        'NAME': 'Vozila',  # Naziv baze na serveru
+        'NAME': 'IMS_ERP',  # Naziv baze na serveru
         'USER': 'Rajo Milicevic',
         'PASSWORD': 'Rajo123',
         'HOST': 'SMS-SERVER',
